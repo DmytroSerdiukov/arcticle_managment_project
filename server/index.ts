78ba4bd0b715fe7cee44544f59874a15effde413
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-03 12:02:28 +0300

    refactor: server routes and db con

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import posts from "./routes/posts";
+import auth from "./routes/auth";
+import connectDB from "./config/db";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", posts);
+app.use("/", auth);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+connectDB();
