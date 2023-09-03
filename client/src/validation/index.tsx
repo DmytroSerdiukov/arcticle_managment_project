3052c3d0cbc4b33b32470011ee395e4127ec5ac4
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-03 10:02:00 +0300

    feature: zod form validation

---
+++ client/src/validation/index.tsx
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const schema = z.object({
+  login: z.string().nonempty(),
+  password: z.string().nonempty(),
+  rememberMe: z.boolean().optional(),
+});
+
+export const PostSchema = z.object({
+  title: z.string().nonempty(),
+  description: z.string().nonempty(),
+});
